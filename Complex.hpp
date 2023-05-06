a0751780c312a937d7b8303762b969e27a2c4306
Farzan Mirshekari <farzanmirshekari@gmail.com>
2023-05-05 23:15:11 -0400

    Member function declarations for Complex, some may be redundant..

--- Complex.hpp
+++ Complex.hpp
@@ -13,6 +13,19 @@ class Complex {
         Complex(sf::Vertex point);    
         Complex(double real, double imaginary);
         sf::Vector2f get_vector();
+        friend Complex operator*(Complex a, Complex b);
+        friend Complex operator+(Complex a, Complex b);
+        friend Complex operator-(Complex a, Complex b);
+        friend Complex operator/(Complex a, Complex b);
+        friend void operator*=(Complex &a, Complex b);
+        friend void operator+=(Complex &a, Complex b);
+        friend void operator-=(Complex &a, Complex b);
+        friend void operator/=(Complex &a, Complex b);
+        void print();
+        Complex vector_to_complex(sf::Vector2f point);
+        Complex vertex_to_complex(sf::Vertex point);
+        sf::Vector2f complex_to_vector(Complex complex);
+        Complex exponentiate();
 };
 
 #endif
