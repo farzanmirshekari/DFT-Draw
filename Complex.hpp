0764bf9987d9b904812a7ba94db052898db41096
Farzan Mirshekari <farzanmirshekari@gmail.com>
2023-05-06 19:30:35 -0400

    Restructure of Complex class..

--- Complex.hpp
+++ Complex.hpp
@@ -4,28 +4,13 @@
 #include <SFML/Graphics.hpp>
 
 class Complex {
-    private:
+    public:
         double real;
         double imaginary;
-
-    public:
         Complex(sf::Vector2f point);
         Complex(sf::Vertex point);    
         Complex(double real, double imaginary);
         sf::Vector2f get_vector();
-        friend Complex operator*(Complex a, Complex b);
-        friend Complex operator+(Complex a, Complex b);
-        friend Complex operator-(Complex a, Complex b);
-        friend Complex operator/(Complex a, Complex b);
-        friend void operator*=(Complex &a, Complex b);
-        friend void operator+=(Complex &a, Complex b);
-        friend void operator-=(Complex &a, Complex b);
-        friend void operator/=(Complex &a, Complex b);
-        void print();
-        Complex vector_to_complex(sf::Vector2f point);
-        Complex vertex_to_complex(sf::Vertex point);
-        sf::Vector2f complex_to_vector(Complex complex);
-        Complex exponentiate();
 };
 
 #endif
