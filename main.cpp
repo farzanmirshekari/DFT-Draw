d705e8d05ff324bedf00dca70f6fe579dce1b20a
Farzan Mirshekari <farzanmirshekari@gmail.com>
2023-05-07 13:08:43 -0400

    main driver added..

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include "draw.hpp"
+
+int main(int argc, char** argv) {
+    std::string file_name = std::string(argv[1]);
+
+    float zoom = 4.0f;
+    int x_offset = 0.0f, y_offset = 0.0f;
+
+    for (int i = 0; i < argc; i++) {
+        if (std::string(argv[i]) == "-z") {
+            zoom = std::stof(argv[i + 1]);
+        }
+    }
+    
+    draw(file_name, zoom, x_offset, y_offset);
+}
