d26f38d32bfd55d2ceda823fd376d8fdba6750f0
Farzan Mirshekari <farzanmirshekari@gmail.com>
2023-05-16 21:24:17 -0400

    Write calculated coefficients to file..

--- dft.hpp
+++ dft.hpp
@@ -6,11 +6,13 @@
 
 #include <SFML/Graphics.hpp>
 #include <cmath>
+#include <fstream>
 
 using namespace std;
 using namespace sf;
 
 void discrete_fourier_transform(Polygon figure, map<int, Complex>& coefficient) {
+	ofstream outfile("coefficients.txt");
 	for (int coefficient_index = MIN_FREQUENCY; coefficient_index <= MAX_FREQUENCY; coefficient_index++) {
 		coefficient[coefficient_index] = Complex(0, 0);
 		int i = 0;
@@ -20,5 +22,7 @@ void discrete_fourier_transform(Polygon figure, map<int, Complex>& coefficient) 
 				exponentiate(Complex(0, -2 * M_PI * _t * coefficient_index)) * 
 				_dt;
 		}
+		outfile << coefficient[coefficient_index].real << " + " << coefficient[coefficient_index].imaginary << "j" << " \n";
 	}
+	outfile.close();
 }
