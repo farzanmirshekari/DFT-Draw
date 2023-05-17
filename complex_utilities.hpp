5bc79588004d41cc12003129c37b878c7f68ed4e
Farzan Mirshekari <farzanmirshekari@gmail.com>
2023-05-16 21:11:45 -0400

    Complex-related functions made inline..

--- complex_utilities.hpp
+++ complex_utilities.hpp
@@ -3,11 +3,11 @@
 #include <cmath>
 #include <iostream>
 
-Complex operator*(Complex& a, Complex &b) {
+inline Complex operator*(Complex& a, Complex &b) {
     return Complex(a.real * b.real - a.imaginary * b.imaginary, a.real * b.imaginary + a.imaginary * b.real);
 }
 
-Complex operator*(Complex const a, Complex const b) {
+inline Complex operator*(Complex const a, Complex const b) {
     return Complex(a.real * b.real - a.imaginary * b.imaginary, a.real * b.imaginary + a.imaginary * b.real);
 }
 
@@ -15,66 +15,66 @@ Complex operator+(Complex& a, Complex &b) {
     return Complex(a.real + b.real, a.imaginary + b.imaginary);
 }
 
-Complex operator+(Complex const a, Complex const b) {
+inline Complex operator+(Complex const a, Complex const b) {
     return Complex(a.real + b.real, a.imaginary + b.imaginary);
 }
 
-void operator+=(Complex& a, Complex &b) {
+inline void operator+=(Complex& a, Complex &b) {
     a.real += b.real;
     a.imaginary += b.imaginary;
 }
 
-void operator+=(Complex& a, Complex const b) {
+inline void operator+=(Complex& a, Complex const b) {
     a.real += b.real;
     a.imaginary += b.imaginary;
 }
 
-void operator*=(Complex& a, Complex &b) {
+inline void operator*=(Complex& a, Complex &b) {
     a = Complex(a.real * b.real - a.imaginary * b.imaginary, a.real * b.imaginary + a.imaginary * b.real);
 }
 
-Complex operator*(Complex& a, double b) {
+inline Complex operator*(Complex& a, double b) {
     return Complex(a.real * b, a.imaginary * b);
 }
 
-Complex operator*(Complex const a, double b) {
+inline Complex operator*(Complex const a, double b) {
     return Complex(a.real * b, a.imaginary * b);
 }
 
-Complex operator+(Complex& a, double b) {
+inline Complex operator+(Complex& a, double b) {
     return Complex(a.real + b, a.imaginary);
 }
 
-Complex operator+(Complex const a, double b) {
+inline Complex operator+(Complex const a, double b) {
     return Complex(a.real + b, a.imaginary);
 }
 
-void operator*= (Complex& a, double b) {
+inline void operator*= (Complex& a, double b) {
     a.real *= b;
     a.imaginary *= b;
 }
 
-void operator+= (Complex& a, double b) {
+inline void operator+= (Complex& a, double b) {
     a.real += b;
 }
 
-void print(Complex a) {
+inline void print(Complex a) {
     std::cout << a.real << " + " << a.imaginary << "i" << std::endl;
 }
 
 
-Complex vector_to_complex(sf::Vector2f point) {
+inline Complex vector_to_complex(sf::Vector2f point) {
 	return Complex(point);
 }
 
-Complex vertex_to_complex(sf::Vertex point) {
+inline Complex vertex_to_complex(sf::Vertex point) {
 	return Complex(point);
 }
 
-sf::Vector2f complex_to_vector(Complex number) {
+inline sf::Vector2f complex_to_vector(Complex number) {
 	return number.get_vector();
 }
 
-Complex exponentiate(Complex c) {
+inline Complex exponentiate(Complex c) {
 	return Complex(cos(c.imaginary), sin(c.imaginary)) * exp(c.real);
 }
